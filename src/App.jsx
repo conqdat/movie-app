291d7326acc83d21d9a53b877553eacff286896f
conqdat <trancongdat.datct@gmail.com>
2024-07-29 10:22:34 +0700

    add config prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './index.css'
+import "./index.css";
 
 function App() {
   return (
     <>
       <div>
-        <header className="h-14 bg-slate-950 flex justify-between text-white items-center px-8">
+        <header className="flex h-14 items-center justify-between bg-slate-950 px-8 text-white">
           <div className="flex items-center gap-4">
             <img src="netflix.png" className="w-16 sm:w-28" />
             <a href="#">Phim</a>
@@ -14,7 +14,7 @@ function App() {
         </header>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
