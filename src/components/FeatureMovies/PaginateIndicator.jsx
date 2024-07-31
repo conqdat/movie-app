bc7bb6c10b1728158455b40f7129efc486328404
conqdat <trancongdat.datct@gmail.com>
2024-07-31 23:18:41 +0700

    add pagination

--- src/components/FeatureMovies/PaginateIndicator.jsx
+++ src/components/FeatureMovies/PaginateIndicator.jsx
@@ -1,13 +1,23 @@
-const PaginateIndicator = () => {
+const PaginateIndicator = ({ movies, activeMovieId, setActiveMovieId }) => {
+  const handleClick = (movie) => {
+    setActiveMovieId(movie.id);
+  };
+
   return (
     <div className="absolute bottom-[10%] right-8">
       <ul className="flex gap-1">
-        <li className="h-1 w-6 cursor-pointer bg-slate-100"></li>
-        <li className="h-1 w-6 cursor-pointer bg-slate-600"></li>
-        <li className="h-1 w-6 cursor-pointer bg-slate-600"></li>
-        <li className="h-1 w-6 cursor-pointer bg-slate-600"></li>
+        {movies.map((movie) => {
+          return (
+            <li
+              key={movie.id}
+              className={`h-1 w-6 cursor-pointer ${movie.id === activeMovieId ? "bg-slate-100" : "bg-slate-600"}`}
+              onClick={() => handleClick(movie)}
+            ></li>
+          );
+        })}
       </ul>
     </div>
   );
 };
+
 export default PaginateIndicator;
--- src/components/FeatureMovies/index.jsx
+++ src/components/FeatureMovies/index.jsx
@@ -1,31 +1,54 @@
 import PaginateIndicator from "./PaginateIndicator";
 import Movie from "./Movie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const FeatureMovies = () => {
   const [movies, setMovies] = useState([]);
-  console.log('Rendering');
+  const [activeMovieId, setActiveMovieId] = useState();
 
-  // fetch("https://api.themoviedb.org/3/movie/popular", {
-  //   method: "GET",
-  //   headers: {
-  //     accept: "application/json",
-  //     Authorization:
-  //       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjdiZDc2MzY5NjRlMDNiMWM1OGU4MTM3NDgyNzk4NCIsIm5iZiI6MTcyMDE4ODA4OS43NDAyNzQsInN1YiI6IjY2ODdmYjQ0YzA4NjJhYmVlYWQ0ZGVjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4gCFYDDyvipIesrHjs10NCVvhLG_jdh33R9uXj4VBaI",
-  //   },
-  // }).then(async (res) => {
-  //   const data = await res.json();
-  //   console.log({data});
-  //   setMovies(data.results);
-  // });
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjdiZDc2MzY5NjRlMDNiMWM1OGU4MTM3NDgyNzk4NCIsIm5iZiI6MTcyMDE4ODA4OS43NDAyNzQsInN1YiI6IjY2ODdmYjQ0YzA4NjJhYmVlYWQ0ZGVjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4gCFYDDyvipIesrHjs10NCVvhLG_jdh33R9uXj4VBaI",
+      },
+    }).then(async (res) => {
+      const data = await res.json();
+      const popularMovies = data.results.slice(0, 4);
+      setMovies(popularMovies);
+      setActiveMovieId(popularMovies[0]?.id);
+    });
+  }, []);
 
-  // console.log(movies);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveMovieId((prevId) => {
+        const currentIndex = movies.findIndex((movie) => movie.id === prevId);
+        const nextIndex = (currentIndex + 1) % movies.length;
+        return movies[nextIndex].id;
+      });
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [movies]);
 
   return (
     <div className="relative text-white">
-      <Movie />
-      <PaginateIndicator />
+      {movies
+        .filter((movie) => movie.id === activeMovieId)
+        .map((movie) => (
+          <Movie key={movie.id} data={movie} />
+        ))}
+
+      <PaginateIndicator
+        movies={movies}
+        activeMovieId={activeMovieId}
+        setActiveMovieId={setActiveMovieId}
+      />
     </div>
   );
 };
+
 export default FeatureMovies;
