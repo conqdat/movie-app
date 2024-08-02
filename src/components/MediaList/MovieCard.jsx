9102a5f89aef5b2d77f0cb697df4802b96a9707d
conqdat <trancongdat.datct@gmail.com>
2024-08-03 00:03:41 +0700

    add circle

--- src/components/MediaList/MovieCard.jsx
+++ src/components/MediaList/MovieCard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import CircularProgressBar from "./CircularProgressBar";
 
 const MovieCard = () => {
   return (
-    <div>
+    <div className="rounded-lg border border-slate-800">
       <img
         className="rounded-lg"
         src={`https://image.tmdb.org/t/p/original/tncbMvfV0V07UZozXdBEq4Wu9HH.jpg`}
       />
-      <div>
-        <p>69</p>
+      <div className="px-4 py-2">
+        <CircularProgressBar progress={10} />
+        <p className="mt-2 font-bold">King of the Planet of the apes</p>
+        <p className="text-slate-300">2024-05-08</p>
       </div>
     </div>
   );
